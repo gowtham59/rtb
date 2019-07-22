87097ca83c15d51026c87f03b078aa9ff58485e6
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-22 16:15:09 +0530

    Create r.py

---
+++ r.py
@@ -0,0 +1,7 @@
+a1,b2=map(int,input().split())
+c3,d4=map(int,input().split())
+x1,y2=map(int,input().split())
+if(a1==b2,c3==d4,x1==y2,a1==c3==x,b2==d4==y2):
+    print("yes")
+else:
+    print("no")
